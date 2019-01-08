27931e08ffe8301784f3b85cdd35fd6e53d278ed
Jon Fleming <jon.fleming@hotmail.com>
2019-01-07 16:08:56 -0800

    add missing flags

--- oauth2proxy.sh
+++ oauth2proxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/etc/oauth2_proxy -upstream=http://0.0.0.0:3000 \
+/etc/oauth2_proxy -email-domain=* -upstream=http://52.246.254.233:4000 -cookie-secret=12345678 \
 -client-id=daa19535-fa14-4b4c-9cd4-d5511fcb9273 \
 -client-secret=NUP5MEqlZQBOBkJu/+twgp4tSJmSajqsIzA3MCMDzog= \
 -provider=azure \
