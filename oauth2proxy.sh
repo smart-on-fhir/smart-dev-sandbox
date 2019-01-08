9afb11a1e49734b0245288580582ce0060474a95
Jon Fleming <jon.fleming@hotmail.com>
2019-01-07 16:04:49 -0800

    path of oath2_proxy

--- oauth2proxy.sh
+++ oauth2proxy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-oauth2_proxy -email-domain=domain.com -upstream=http://0.0.0.0:3000 -cookie-secret=12345678
--client-id=daa19535-fa14-4b4c-9cd4-d5511fcb9273
--client-secret=NUP5MEqlZQBOBkJu/+twgp4tSJmSajqsIzA3MCMDzog=
--provider=azure
--cookie-secure=false
+/etc/oauth2_proxy -upstream=http://0.0.0.0:3000 \
+-client-id=daa19535-fa14-4b4c-9cd4-d5511fcb9273 \
+-client-secret=NUP5MEqlZQBOBkJu/+twgp4tSJmSajqsIzA3MCMDzog= \
+-provider=azure \
+-cookie-secure=false \
 -cookie-expire=0h15m0s &
 
-service nginx restart
+docker restart nginx
