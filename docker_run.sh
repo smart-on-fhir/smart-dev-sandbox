2c8d427399fa1308e2c3290adb2ad044278c4db9
Jon Fleming <jon.fleming@hotmail.com>
2019-01-14 12:43:55 -0800

    add basic auth to port 4000

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,19 @@
-sudo docker run -p 80:80 -p 443:443 -p 4000-4005:4000-4005 --name nginx -v /home/azure/smart-dev-sandbox/nginx/conf.d:/etc/nginx/conf.d -v /home/azure/smart-dev-sandbox/nginx/modules:/etc/nginx/modules -v /home/azure/html:/usr/share/nginx/html -v /home/azure/certs:/etc/letsencrypt -d nginx:alpine
+sudo docker run -p 80:80 -p 443:443 -p 4000-4005:4000-4005 --name nginx \
+-v /home/azure/smart-dev-sandbox/nginx/conf.d:/etc/nginx/conf.d \
+-v /home/azure/smart-dev-sandbox/nginx/modules:/etc/nginx/modules \
+-v /home/azure/html:/usr/share/nginx/html \
+-v /home/azure/certs:/etc/letsencrypt -d nginx:alpine
+
+sudo docker run -p 4180:4180 -p 8080:8080 --name oauth2_proxy -d a5huynh/oauth2_proxy \
+--upstream=http://fhir.smart.mcg.com:4002 \
+--http-address="0.0.0.0:4180" \
+--cookie-secret=12345678 \
+--client-id=daa19535-fa14-4b4c-9cd4-d5511fcb9273 \
+--client-secret=NUP5MEqlZQBOBkJu/+twgp4tSJmSajqsIzA3MCMDzog= \
+--provider=azure \
+--cookie-secure=false \
+--email-domain="mcg.com" \
+--cookie-expire=0h15m0s
+
+
 
--- oauth2proxy.sh
+++ oauth2proxy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-/etc/oauth2_proxy -email-domain=* -upstream=http://52.246.254.233:4000 -cookie-secret=12345678 \
--client-id=daa19535-fa14-4b4c-9cd4-d5511fcb9273 \
--client-secret=NUP5MEqlZQBOBkJu/+twgp4tSJmSajqsIzA3MCMDzog= \
--provider=azure \
--cookie-secure=false \
--cookie-expire=0h15m0s &
+docker run -p 4180:4180 a5huynh/oauth2_proxy \
+--upstream=http://fhir.smart.mcg.com:4002 \
+--cookie-secret=12345678 \
+--client-id=daa19535-fa14-4b4c-9cd4-d5511fcb9273 \
+--client-secret=NUP5MEqlZQBOBkJu/+twgp4tSJmSajqsIzA3MCMDzog= \
+--provider=azure \
+--cookie-secure=false \
+--email-domain="*" \
+--cookie-expire=0h15m0s 
 
 docker restart nginx
+ssh azure@13.77.159.104
+
+
+docker run -e OAUTH2_PROXY_COOKIE_SECRET=<cookie-secret> \
+    -e OAUTH2_PROXY_CLIENT_ID=<client-id> \
+    -e OAUTH2_PROXY_CLIENT_SECRET=<client-secret> \
+    a5huynh/oauth2_proxy
