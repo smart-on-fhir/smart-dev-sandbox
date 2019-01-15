dba98c16627afcf93ef7628a47c0888f89acee75
Jon Fleming <jon.fleming@hotmail.com>
2019-01-14 16:36:11 -0800

    authserver

---
+++ authserver.py
@@ -0,0 +1,30 @@
+from bottle import route, run, template, get, post, request
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+@get('/login') # or @route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@post('/login') # or @route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+def check_login(username, password):
+    return True
+
+
+run(host='localhost', port=4190)
