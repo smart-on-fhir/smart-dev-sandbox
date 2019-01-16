ae1aae9cfdbd1b24005b3a6270246ceedcb732d5
Jon Fleming <jon.fleming@hotmail.com>
2019-01-16 13:07:08 -0800

    Validate jwt

--- authserver.js
+++ authserver.js
@@ -1,13 +1,12 @@
-var express     = require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
+const express     = require('express');
+const app         = express();
+const bodyParser  = require('body-parser');
+const jwt         = require('jsonwebtoken');
+const request     = require('request');
+const jwkToPem    = require('jwk-to-pem');
 
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-var User   = require('./app/models/user'); // get our mongoose model
-
-var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-app.set('superSecret', config.secret); // secret variable
+const port = process.env.PORT || 8080; 
+app.set('superSecret', 'NUP5MEqlZQBOBkJu/+twgp4tSJmSajqsIzA3MCMDzog='); 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,9 +16,37 @@ app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
+app.post('/oauth2/auth', function(req, res, next){
+    const token = req.headers["authorization"].replace(/bearer /i,'');
+    const decoded = jwt.decode(token, {complete: true});
+    
+    function extractKeys(error, response, body) {
+        if(error)
+            throw new Error(error);
+    
+        const jwk_keys = JSON.parse(response.body);
+        let keys = jwk_keys.keys.filter((x) => x['kid'] === decoded.header['kid']);
+        
+        if (keys.length)
+            validateToken(keys[0]);
+    }
 
+    function validateToken(key) {
+        console.log(decoded.header);
+        const pem = jwkToPem(key);
 
+        jwt.verify(token, pem, function(err, decoded) {
+            if(err)
+                res.status(403).send(err);
+            
+            return(res.status(200).send('OK'));
+        })
+    }
+    request('https://login.windows.net/common/discovery/keys',extractKeys)
+
+});
 
 
 app.listen(port);
 console.log('listening on http://localhost:' + port);
+
